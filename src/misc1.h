bb77fb1be65f25c7fe2e466f417e1fee85fef16f
Wolfgang Schwotzer <wolfgang.schwotzer@gmx.net>
2024-02-17 17:41:13 +0100

    Update automake/autoconf files.
    - Update Makefile.am
    - Update configure.ac
    - Update m4 scripts
    - Remove SDL (version 1.2) dependency.

--- src/misc1.h
+++ src/misc1.h
@@ -43,16 +43,10 @@
 #endif
 
 /* time */
-#if TIME_WITH_SYS_TIME
+#if HAVE_SYS_TIME_H
     #include <sys/time.h>
-    #include <time.h>
-#else
-    #if HAVE_SYS_TIME_H
-        #include <sys/time.h>
-    #else
-        #include <time.h>
-    #endif
 #endif
+#include <time.h>
 
 #ifdef _WIN32
     /* Name of package */
